2cf8f76b6cee1e8d43ce148966902621ad340a54
Miguel Macicior <mimacici@ucm.es>
2025-04-29 15:08:27 +0200

    maquinavs con iconos

--- QQSM/pages/openAIIA_page.py
+++ QQSM/pages/openAIIA_page.py
@@ -1,20 +1,29 @@
 import reflex as rx
-from QQSM.styles.colors import Colors
 from QQSM.states.game_state import GameState
+from QQSM.styles.colors import Colors
+from QQSM.pages.components import *
 
 
-@rx.page(route="/openAI")
+@rx.page(route="/game")
 def openaiia_page():
     return rx.box(
         render_upper_panel(),
-        render_progress_indicator(),
+        # En función del número de circulitos
+        render_progress_indicator(15),
 
         rx.hstack(
-            render_question_section(),
+            render_central_panel(),
             spacing="6",
             align="start",
             justify="center"
         ),
+
+        render_lower_panel(),
+
+        rx.box(render_left_panel(), position="absolute", left="0px", bottom="15px"),
+        rx.box(render_right_panel(), position="absolute", right="50px", bottom="50px"),
+
+        render_feedback_line(),
 
         width="100vw",
         min_height="100vh",
@@ -29,7 +38,7 @@ def openaiia_page():
 def render_upper_panel():
     return rx.hstack(
         render_exit_button(),
-        render_game_header(),
+        render_game_header("¿QUIÉN QUIERE SER MILLONARIO?"),
         render_next_button(),
         width="100%",
         padding="0px 20px",
@@ -38,79 +47,7 @@ def render_upper_panel():
     )
 
 
-def render_game_header():
-    return rx.box(
-        rx.text(
-            "¿QUIÉN QUIERE SER MILLONARIO? OpenAI vs Gemini",
-            class_name="title-style",
-            margin="0px"
-        ),
-        width="100%",
-        padding="0px",
-        text_align="center"
-    )
-
-
-def render_exit_button():
-    return rx.button(
-        "✖",
-        on_click=rx.redirect("/menu"),
-        class_name="menu-exit-button"
-    )
-
-
-def render_next_button():
-    return rx.cond(
-        GameState.correct_answer,
-        rx.button(
-            "➜",
-            on_click=GameState.next_round,
-            class_name="next-arrow-button"
-        )
-    )
-
-
-def get_gradient_color(index):
-    factor = index / 14
-    r = int(0 + factor * 255)
-    g = int(255 - factor * 255)
-    b = 0
-    return f"rgb({r},{g},{b})"
-
-
-def render_progress_indicator():
-    return rx.hstack(
-        *[
-            rx.box(
-                rx.text(str(i + 1), font_size="14px", color="gold", text_align="center"),
-                rx.box(
-                    class_name=rx.cond(
-                        i == GameState.number_question - 1,
-                        "progress-circle current",
-                        "progress-circle"
-                    ),
-                    background_color=rx.cond(
-                        i < GameState.number_question,
-                        get_gradient_color(i),
-                        "gray"
-                    )
-                ),
-                display="flex",
-                flex_direction="column",
-                align_items="center",
-            )
-            for i in range(15)
-        ],
-        spacing="6",
-        align="center",
-        justify="center",
-        class_name="progress-container",
-        width="100%",
-        margin_bottom="5px"
-    )
-
-
-def render_question_section():
+def render_central_panel():
     return rx.vstack(
         rx.hstack(
             render_question_title(),
@@ -129,57 +66,72 @@ def render_question_section():
     )
 
 
-def render_question_title():
+def render_left_panel():
     return rx.box(
-        rx.text(
-            GameState.question,
-            white_space="normal",
-            word_break="break-word",
-            color="white",
-            text_align="center",
-            class_name="custom-title",
+        rx.cond(
+            GameState.show_call_box,
+            render_call_box()
         ),
-        width="70%",
-        padding="5px",
-        border_radius="8px",
+        width="25vw",
+        height="55vh",
+        background_color="transparent",
     )
 
 
-def render_question_topic():
+
+def render_right_panel():
     return rx.box(
-        rx.text(GameState.topic, class_name="custom-category"),
-        width="100px",
-        height="100px",
-        display="flex",
-        align_items="center",
-        justify_content="center",
-        border_radius="50%",
-        background_color=Colors.GOLD,
-        flex_shrink="0"
+        rx.cond(
+            GameState.public_used & (GameState.public_items != []),
+            render_public_chart()
+        ),
+        width="25vw",
+        height="33vh",
+        background_color="transparent",
     )
 
 
-def render_answer_options():
-    return rx.grid(
-        *[
-            rx.button(
-                f"{letter}) {getattr(GameState, f'option_{letter.lower()}')}",
-                width="100%",
-                font_size="14px",
-                white_space="normal",
-                word_break="break-word",
-                height="auto",
-                min_height="50px",
-                class_name=GameState.button_classes[letter],
-                on_click=lambda le=letter: GameState.validate_answer(le),
-                disabled=GameState.chosen_answer
-            )
-            for letter in ["A", "B", "C", "D"]
-        ],
-        columns="2",
-        spacing="4",
-        width="70%",  
-        align_items="center",
-        justify_content="center",
-        margin_top="10px",
+
+def render_lower_panel():
+    return rx.center(
+        rx.hstack(
+            rx.box(
+                class_name="theme-icon image",
+                background_image="url('/ias/gemini.png')",
+                **{"data-theme": "gemini"},
+                width="100px",
+                height="100px",
+            ),
+            rx.text(
+                "VS",
+                font_size="4em",
+                font_weight="bold",
+                font_family="Impact, sans-serif",
+                color="transparent",
+                background="linear-gradient(180deg, #FFD700, #FF4500, #8B0000)",
+                background_clip="text",
+                text_shadow=(
+                    "1px 1px 1px black, "
+                    "0 0 4px #FF4500, "
+                    "0 0 8px #FF8C00"
+                ),
+                margin="0 20px",
+                height="100px",
+                line_height="100px",
+                display="flex",
+                align_items="center",
+                justify_content="center",
+            ),
+
+            rx.box(
+                class_name="theme-icon image",
+                background_image="url('/ias/openAI.png')",
+                **{"data-theme": "openAI"},
+                width="100px",
+                height="100px",
+            ),
+            spacing="6",
+            margin_top="10px"
+        ),
+        width="100%"
     )
